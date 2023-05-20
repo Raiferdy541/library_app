c2e4486aa89d2c73966ec14958f062b1b815dedf
Raiferdy541 <raihan.ferdyanza@gmail.com>
2023-05-20 22:43:35 +0700

    ganti url

--- deployData.js
+++ deployData.js
@@ -10,6 +10,6 @@ const studentName = 'Raihan Ferdyanza'; // TODO: replace this
 
 const studentId = 'FE5235894'; // TODO: replace this
 
-const NetlifyDeployUrl = 'libraryapp-raihanferdyanza.netlify.app'; // TODO: replace this
+const NetlifyDeployUrl = 'https://libraryapp-raihanferdyanza.netlify.app/client/index.html'; // TODO: replace this
 
 module.exports = { studentName, studentId, NetlifyDeployUrl };
